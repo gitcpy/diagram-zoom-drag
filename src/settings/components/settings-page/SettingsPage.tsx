3b6599b5e79a5340ea1f9faaf23db38f96dba113
Sentiago <arseny317@gmail.com>
2024-10-28 20:23:28 +0300

    feat: use React Memory Router for tab management
    - render navbar and reset button differently based on the current platform
    - render routes and page elements for all main navbar tabs
    - add pages: General, Diagram section, Panel section, About

--- src/settings/components/settings-page/SettingsPage.tsx
+++ src/settings/components/settings-page/SettingsPage.tsx
@@ -1,27 +1,58 @@
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Navbar from '../navbar/Navbar';
-import { useSettingsContext } from '../core/context';
 import General from '../pages/general/General';
-import DiagramManagement from '../pages/diagram-management/DiagramManagement';
+import ResetSettings from './reset-settings/ResetSettings';
+import { useSettingsContext } from '../core/context';
+import About from '../pages/about/About';
+import DiagramSection from '../pages/diagram-section/DiagramSection';
+import PanelSection from '../pages/panel-section/PanelSection';
+import { Platform } from 'obsidian';
 
 const SettingsPage: React.FC = () => {
-    const { currentTab } = useSettingsContext();
-
-    const renderContent = () => {
-        switch (currentTab) {
-            case 'general':
-                return <General />;
-            case 'diagram-management':
-                return <DiagramManagement />;
-        }
-    };
-
+    const { reloadCount, currentPath } = useSettingsContext();
     return (
-        <>
-            <Navbar />
-            {renderContent()}
-        </>
+        <MemoryRouter initialEntries={[currentPath]} key={reloadCount}>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    position: 'relative',
+                    width: '100%',
+                    ...(Platform.isMobileApp
+                        ? {
+                              flexDirection: 'column',
+                              gap: '16px',
+                          }
+                        : {}),
+                }}
+            >
+                <Navbar />
+                <div
+                    style={{
+                        ...(Platform.isMobileApp
+                            ? {
+                                  position: 'static',
+                                  marginTop: '16px',
+                              }
+                            : {
+                                  position: 'absolute',
+                                  right: 0,
+                                  marginTop: '30px',
+                              }),
+                    }}
+                >
+                    <ResetSettings />
+                </div>
+            </div>
+            <Routes>
+                <Route path="/general" element={<General />} />
+                <Route path={'/diagram-section'} element={<DiagramSection />} />
+                <Route path="/panel-section" element={<PanelSection />} />
+                <Route path={'/about'} element={<About />} />
+            </Routes>
+        </MemoryRouter>
     );
 };
-
 export default SettingsPage;
