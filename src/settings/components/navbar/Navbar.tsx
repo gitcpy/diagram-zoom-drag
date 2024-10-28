cc2f0603e0373ff28d15bafb293a4bf850876611
Sentiago <arseny317@gmail.com>
2024-10-28 20:14:29 +0300

    feat: use NavLinks in navbar

--- src/settings/components/navbar/Navbar.tsx
+++ src/settings/components/navbar/Navbar.tsx
@@ -1,26 +1,22 @@
+import { NavLink } from 'react-router-dom';
 import React from 'react';
-import { useSettingsContext } from '../core/context';
 import { NavbarContainer, NavbarTab, NavbarTabs } from './styled';
 
 const Navbar: React.FC = () => {
-    const { currentTab, setCurrentTab } = useSettingsContext();
-
     return (
         <NavbarContainer>
             <NavbarTabs>
-                <NavbarTab
-                    className={currentTab === 'general' ? 'active' : ''}
-                    onClick={() => setCurrentTab('general')}
-                >
+                <NavbarTab as={NavLink} to="/general">
                     General
                 </NavbarTab>
-                <NavbarTab
-                    className={
-                        currentTab === 'diagram-management' ? 'active' : ''
-                    }
-                    onClick={() => setCurrentTab('diagram-management')}
-                >
-                    Diagram management
+                <NavbarTab as={NavLink} to={'/diagram-section'}>
+                    Diagram
+                </NavbarTab>
+                <NavbarTab as={NavLink} to={'/panel-section'}>
+                    Panel
+                </NavbarTab>
+                <NavbarTab as={NavLink} to={'/about'}>
+                    About
                 </NavbarTab>
             </NavbarTabs>
         </NavbarContainer>
