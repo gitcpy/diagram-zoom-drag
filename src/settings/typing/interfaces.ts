ea019570b8e4c361cf2149693f54ab3fb0e263ff
Sentiago <arseny317@gmail.com>
2024-10-28 20:07:59 +0300

    feat: update default settings and interface
    - add panels state to each diagram container, allowing user to switch it on/off
    - add panels config for storing general panels state (on/off) and positioning

--- src/settings/typing/interfaces.ts
+++ src/settings/typing/interfaces.ts
@@ -2,10 +2,34 @@ export interface DiagramData {
     name: string;
     selector: string;
     on: boolean;
+    panels: {
+        [key: string]: {
+            on: boolean;
+        };
+    };
+}
+
+export interface PanelPosition {
+    top?: string;
+    bottom?: string;
+    left?: string;
+    right?: string;
+}
+
+export interface PanelConfig {
+    enabled: boolean;
+    position: PanelPosition;
+}
+
+export interface PanelsConfig {
+    service: PanelConfig;
+    move: PanelConfig;
+    zoom: PanelConfig;
 }
 
 export interface DefaultSettings {
     supported_diagrams: DiagramData[];
+    panelsConfig: PanelsConfig;
     diagramsPerPage: number;
     foldingByDefault: boolean;
     automaticFoldingOnFocusChange: boolean;
