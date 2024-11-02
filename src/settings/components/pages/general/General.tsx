397ff286cbea97c63e15b3dfd31c97f858d11e44
Sentiago <arseny317@gmail.com>
2024-11-02 10:15:43 +0300

    feat: remove General settings page

--- src/settings/components/pages/general/General.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useSettingsContext } from '../../core/context';
-
-const General: React.FC = () => {
-    const { app, plugin } = useSettingsContext();
-    return <div></div>;
-};
-
-export default General;
